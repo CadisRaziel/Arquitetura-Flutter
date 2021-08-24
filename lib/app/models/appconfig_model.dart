9c129db4ba570f37d1eba20918e259047c8a64e0
CadisRaziel <vitorcsharp@hotmail.com>
2021-08-24 20:31:22 -0300

    #02 models e facilitação de caminho do código

---
+++ lib/app/models/appconfig_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppConfigModel {
+  //!Aqui vai ficar algumas configurações do app todo
+
+  //*Adicionando o valueNofitier aqui e tirando do app_controller.dart
+   final themeSwitch = ValueNotifier<bool>(false);
+}
